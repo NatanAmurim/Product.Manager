f5127973f29a3fc7afd99ac600d85ad216a89f99
natanamurim <natanamurim1998@gmail.com>
2025-09-02 08:32:41 -0300

    feature: adição de interface no swagger para autenticação.

---
+++ Product.Api/IoC/SwaggerExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace ProductManager.Api.IoC
+{
+    public static class SwaggerExtensions
+    {
+        public static IServiceCollection AddSwaggerInterfaceToAuthenticateWithJWT(this IServiceCollection services)
+        {
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Product Manager", Version = "v1" });
+                
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "Insira o token JWT desta forma: Bearer {seu token}"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
+            });
+
+            return services;
+        }
+    }
+}
